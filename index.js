a072acdce665ea18c84f1f8984b2470620dd0bcd
Alexia Velador <alexiavel01@gmail.com>
2022-03-01 00:30:55 -0700

    commit tree 766f44aca584ba8b2b9920c4a8ecfa39f920e83d

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+// function superbowlWin(record){
+//     //const arr = {year,result}
+//     if(result === "w"){
+//         return year;
+//     }
+//     else{
+//         return 'undefined';
+//     }
+// }
+// array.find(superbowlWin);
+//will have 3 years logged
+// function superbowlWin(year,result){
+//     for(const record of year){
+//         if(record.someWin === result){
+//             console.log(record.year);
+//         }
+//     }
+// }
+// superbowlWin(record, "w")
+
+// function superbowlWin(record){
+//     for(const result of record){
+//         if(result.isWin === "W"){
+//             return(result.year)
+//         }
+//     }
+// }
+// superbowlWin(result);
+
+let foundWin = record.find(superbowlWin)
+console.log(foundWin);
+
+function superbowlWin(record){
+    if(result === 'W'){
+    return record.year;
+    }
+}
